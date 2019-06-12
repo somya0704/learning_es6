69f87ad546bfe769741609f0a1a9b2edff977acc
somya <somya.singhal0797@gmail.com>
2019-06-12 22:09:12 +0530

    block scope

---
+++ blockscopefun.js
@@ -0,0 +1,7 @@
+function foo () { return 1 }
+    foo() === 1
+    {
+        function foo () { return 2 }
+        foo() === 2
+    }
+    foo() === 1
--- blockscopevar.js
+++ blockscopevar.js
@@ -1,12 +1,9 @@
-for (let i=0; i<a.length; i++){
-    let x = a[i]
-    console.log(a[i]);
-}
-for (let i = 0; i < b.length; i++) {
-    let y = b[i]
-}
+
+
+
 
 let callbacks = []
 for (let i = 0; i <= 2; i++) {
     callbacks[i] = function () { return i * 2 }
+    console.log(callbacks[0]);
 }
